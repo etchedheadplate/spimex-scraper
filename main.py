c7b97b88ec1d790f30d34641b4c28ee14d618a24
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-09-14 16:16:08 +0300

    Импортировал модули

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.loader import SpimexLoader
+from src.models import BaseModel
+from src.parser import SpimexParser
+from src.scraper import SpimexDownloader
 
 load_dotenv()
 
@@ -9,3 +17,20 @@ DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+BaseModel.metadata.create_all(engine)
+
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+start = datetime(2023, 1, 1)
+end = datetime.today()
+downloader = SpimexDownloader(start, end)
+# downloader.run()
+parser = SpimexParser()
+df = parser.parse("bulletins/oil_xls_20230109162000.xls")
+loader = SpimexLoader(session)
+loader.load(df)
