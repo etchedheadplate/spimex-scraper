17a8e43e3f0d51dac0dc651a47f3aae3dfed137a
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-09-14 20:32:46 +0300

    Добавил обработку пустого DataFrame

--- src/loader.py
+++ src/loader.py
@@ -5,16 +5,19 @@ from .models import SpimexTradingResults
 
 
 class SpimexLoader:
-    def __init__(self, session: Session, df: pd.DataFrame):
+    def __init__(self, session: Session, df: pd.DataFrame | None = None) -> None:
         self.session = session
         self.df = df
         self.model = SpimexTradingResults
+        if self.df is None:
+            raise ValueError("[Loader] DataFrame для загрузки отсутствуют.")
 
     def load(self, update_on_conflict: bool = False) -> None:
         model_columns = {c.name for c in self.model.__table__.columns}
 
         df_filtered = self.df.loc[:, self.df.columns.intersection(model_columns)]  # type: ignore
         df_filtered = df_filtered.where(pd.notnull(df_filtered), None)  # type: ignore
+        print(f"[Loader] Получено {len(df_filtered)} строк.")  # type: ignore
 
         records = df_filtered.to_dict(orient="records")  # type: ignore
 
@@ -26,3 +29,4 @@ class SpimexLoader:
             objects = [self.model(**record) for record in records]  # type: ignore
             self.session.add_all(objects)  # type: ignore
             self.session.commit()
+            print(f"[Loader] Загружено {len(objects)} строк.")
