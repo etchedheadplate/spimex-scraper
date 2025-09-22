865c590cb3824f1f8d14696f0113f8b07f739736
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-09-22 23:13:29 +0300

    Создал путь /dates

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,25 +1,41 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import RedirectResponse
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.db_depends import get_async_db
-from src.api.schemas import TradingResults
+from src.api.schemas import TradingDates, TradingResults
 from src.database.models import SpimexTradingResults as TradingModel
 
-router = APIRouter(prefix="/trades", tags=["trades"])
+service_router = APIRouter(tags=["service"])
 
 
-@router.get("/dates")
-async def get_last_trading_dates():
-    pass
+@service_router.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
 
 
-@router.get("/dynamics")
+@service_router.get("/ping")
+async def ping():
+    return {"Response": "pong"}
+
+
+trades_router = APIRouter(prefix="/trades", tags=["trades"])
+
+
+@trades_router.get("/dates", response_model=TradingDates)
+async def get_last_trading_dates(days: int, db: AsyncSession = Depends(get_async_db)):
+    result = await db.scalars(select(TradingModel.date).distinct().order_by(TradingModel.date.desc()).limit(days))
+    dates = result.all()
+    return {"dates": dates}
+
+
+@trades_router.get("/dynamics")
 async def get_dynamics():
     pass
 
 
-@router.get("/results", response_model=list[TradingResults])
+@trades_router.get("/results", response_model=list[TradingResults])
 async def get_trading_results(db: AsyncSession = Depends(get_async_db)):
     result = await db.scalars(select(TradingModel))
     trades = result.all()
