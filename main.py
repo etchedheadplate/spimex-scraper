69b97dba94c48176a21e5b3b4534cb626c79f619
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-09-24 20:59:06 +0300

    Добавил версионирование API

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
 from src.api.routes import trades_router
 
-app = FastAPI(title="Spimex Trading Results API")
+app = FastAPI(title="Spimex API")
 
-app.include_router(trades_router)
+api_v1 = APIRouter(prefix="/v1")
+api_v1.include_router(trades_router)
 
+app.include_router(api_v1)
 
 if __name__ == "__main__":
     import asyncio
